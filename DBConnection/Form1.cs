a7ebee13ef7b64d72157f6ff45e6150a2887d548
denis0872 <denispetergof@gmail.com>
2021-02-14 15:10:21 +0300

    Add project files.

---
+++ DBConnection/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBConnection
+{
+    public partial class Form1 : Form
+    {
+        string testConnect = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;User ID=denis;Initial Catalog=Praktikadb;Data Source=DENIS-COMP\SQLEXPRESS";
+        OleDbConnection connection = new OleDbConnection();
+        public Form1()
+        {
+            InitializeComponent();
+            //connection.StateChange += new
+               // StateChangeEventHandler(
+               // connection_StateChange);
+
+        }
+       /* private void connection_StateChange(object sender, StateChangeEventArgs e)
+        {
+            ToolStripMenuItem.Enabled =
+                (e.CurrentState == ConnectionState.Closed);
+            ToolStripMenuItem.Enabled =
+                (e.CurrentState == ConnectionState.Open);
+        }*/
+
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = testConnect;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных выполнено успешно");
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+            catch (OleDbException XcpSQL)
+            {
+                foreach (OleDbError se in XcpSQL.Errors)
+                {
+                    MessageBox.Show(se.Message, se.SQLState+
+                    "SQL Error code " + se.NativeError,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else
+                MessageBox.Show("Соединение с базой данных уже закрыто");
+
+        }
+    }
+}
